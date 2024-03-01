0a71b4ece5388d59ec1bc6a24fbe976ebdc2cfbf
paul <21paaguiar@gmail.com>
2024-03-01 13:47:58 -0800

    changed the way factors are displayed

--- getFactor.js
+++ getFactor.js
@@ -2,14 +2,13 @@ const prompt = require("prompt-sync")();
 
 var left = prompt("Enter left number: ")
 var right = prompt("Enter right number: "); 
-var mid = 29; 
 
 var big = left * right;
 console.log("Your number is ", big)
 
 var factors = []
 
-function get_factor(num)
+function get_factor(num, mid)
 {
 	for (let i = 1; i <= num; i++)
 	{
@@ -19,11 +18,24 @@ function get_factor(num)
 		}
 	}
 
-	console.log("The factors for ", num, "are: ")
-	for (let i =0; i < factors.length; i++)
+	var l = []
+	var r = []
+
+	for (let k = 0; k < factors.length; k++)
 	{
-		console.log(factors[i])
+		l.push(factors[k])
 	}
+
+	for (let j = factors.length-1; j>=0; j--)
+	{
+		r.push(factors[j])
+	}
+	console.log("Factor pairs are: ")
+	console.log("first #: ")
+	console.log(l)
+	console.log("second #: ")
+	console.log(r)
+	
 }
 
 
